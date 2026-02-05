85c843a1406c5b27a9287d661fa97b234d562926
Angelo Zagada <anazagada@my.cspc.edu.ph>
2026-02-05 12:08:10 +0800

    Second Commit

--- server.js
+++ server.js
@@ -8,10 +8,31 @@ const port = 3000;
 //     res.send("Welcome to express");
 
 // });
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage');
+// app.get('/', (req, res) => {
+//     res.send('GET request to the homepage');
+// }); 
+
+// app.post('/', (req, res) => {
+//     res.send('Post Request');
+// }); 
+
+app.get('/about', (req, res) => {
+    res.send('about page');
 }); 
 
+app.get('/search', (req, res) => {
+    res.send(`User: ${req.query.userId}, 
+             Book ID: ${req.query.bookId || 'none'}`);
+}); 
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    res.send(`User ID: ${req.params.userId}, 
+             Book ID: ${req.params.bookId}`);
+}); 
+
+app.use((req, res) => {
+    res.status(404).send('404 page not found');
+});
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
